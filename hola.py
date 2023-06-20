633ae5805da968c5ae03ab774f45d300d85c334b
Pardo248 <castillopardodi3go@gmail.com>
2023-06-19 22:34:46 -0600

    Hola mundo y tipos de datos

---
+++ hola.py
@@ -0,0 +1,16 @@
+#Hola mundo Para Python
+print("hola mundo") 
+
+"""
+para comentar en varias lineas uwu
+
+"""
+'''
+las comillas simples se pueden usar para lo mismo que las comillas dobles 
+
+'''
+print(type("hola mundo"))#type sirve para consultar el tipo de dato en teoria
+print(type(7))
+print(type(7.5))
+print(type(True))#true no es lo mismo que True
+print(type(1 + 1j))
